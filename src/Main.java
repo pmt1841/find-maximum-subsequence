46ce7d158a11bc9647db76bc31299ebdc7970742
pmt1841 <phamminhtien1841@gmail.com>
2025-11-24 14:21:06 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,27 +10,35 @@ public class Main {
 
         LinkedList<Character> output = new LinkedList<>();
 
-        for (int i = 0; i < input.length(); i++) {
+        for (int i = 0; i < input.length(); i++) {              // Vòng lặp ngoài = n
             LinkedList<Character> temp = new LinkedList<>();
             temp.add(input.charAt(i));
 
-            for (int j = i + 1; j < input.length(); j++) {
+            for (int j = i + 1; j < input.length(); j++) {      // Vòng lặp trong = n-1
                 if (input.charAt(j) > temp.getLast()) {
                     temp.add(input.charAt(j));
                 }
             }
 
-            if (temp.size() > output.size()) {
+            if (temp.size() > output.size()) {                  // Câu lệnh rẽ nhánh = 1
                 output.clear();
                 output.addAll(temp);
             }
-            temp.clear();
+            temp.clear();                                       // Câu lệnh = 1
         }
 
-        for (Character character : output) {
+        for (Character character : output) {                    // Vòng lặp = 1
             System.out.print(character);
         }
 
-        System.out.println();
+        System.out.println();                                   // Câu lệnh = 1
     }
 }
+
+/*
+        ĐỘ PHỨC TẠP CỦA BÀI TOÁN
+    T(n) = n(n - 1) + 1 + 1 + 1 +1
+    T(n) = n^2 - n +4
+    T(n) = O(n^2) - O(n)
+    T(n) = O(n^2)
+ */
